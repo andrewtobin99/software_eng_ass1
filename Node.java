c8cf878f385ed1f89a4c3d544ae9a79161f65eff
andrewtobin99 <72084424+andrewtobin99@users.noreply.github.com>
2020-11-09 18:09:11 +0000

    Update Node.java
    making changes to so ancestors and children are areas of type Node with length of 1.

--- Node.java
+++ Node.java
@@ -2,7 +2,8 @@
 public class Node {
 	char key;
 	int data;
-	Node left, right, ancestors[], children[];
+	Node left, right, 
+	Node[] ancestors, children;
 	
 	Node(char key, int value)
 	{
@@ -10,7 +11,9 @@ public class Node {
 		data = value;
 		left=null;
 		right=null;
-		ancestors=null;
-		children = null;
+		ancestors = new Node[1];
+		children = new Node[1];
+		ancestors[0]=null;
+		children[0]=null;
 	}
 }
